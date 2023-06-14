85606833ecbfaabdca36eea92ec2f247ec691f3a
Edward <116730069+Edward-simsim@users.noreply.github.com>
2023-06-14 12:29:40 +0300

    created forum page and create post page

---
+++ src/app/components/question/question.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.css']
+})
+export class QuestionComponent {
+
+}
