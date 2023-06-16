fc4c882868424f3f36ef382ee3a4ed28a01b0892
Edward <116730069+Edward-simsim@users.noreply.github.com>
2023-06-16 09:51:07 +0300

    update css for create-page,forum-page and question.component ,and created pipes folder

--- src/app/pages/forum-page/forum-page.component.ts
+++ src/app/pages/forum-page/forum-page.component.ts
@@ -9,12 +9,14 @@ import { QuestionService } from "src/app/service/question.service";
   templateUrl: './forum-page.component.html',
   styleUrls: ['./forum-page.component.css']
 })
-export class ForumPageComponent implements OnInit, OnDestroy{
+export class ForumPageComponent implements OnInit, OnDestroy {
+  constructor(private router: Router, private questionService: QuestionService) {}
 
-  constructor(private router: Router, private questionService:QuestionService) {}
-
-  questionSubscription: Subscription= new Subscription();
+  questionSubscription: Subscription = new Subscription();
   questionList: Question[] = [];
+  visibleQuestions: Question[] = [];
+  questionsPerPage = 5;
+  currentPage = 1;
 
   ngOnInit(): void {
     console.log("forum-page init");
@@ -22,22 +24,34 @@ export class ForumPageComponent implements OnInit, OnDestroy{
   }
 
   getQuestions() {
-    console.log("forum-page get quesions()");
+    console.log("forum-page get questions()");
     this.questionSubscription = this.questionService
-    .getQuestions()
-    .subscribe((questions:Question[]) => {
-      this.questionList=questions;
-    })
+      .getQuestions()
+      .subscribe((questions: Question[]) => {
+        this.questionList = questions;
+        this.loadVisibleQuestions();
+      });
   }
 
+  loadVisibleQuestions() {
+    const startIndex = 0;
+    const endIndex = this.currentPage * this.questionsPerPage;
+    this.visibleQuestions = this.questionList.slice(startIndex, endIndex);
+  }
 
+  loadMoreQuestions() {
+    this.currentPage++;
+    this.loadVisibleQuestions();
+  }
 
   test() {
     console.log("merge");
   }
+
   nav_to_create() {
     this.router.navigate(['/create']);
   }
+
   ngOnDestroy(): void {
     this.questionSubscription.unsubscribe();
   }
