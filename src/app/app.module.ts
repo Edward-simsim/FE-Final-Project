232cc4e6515da0450d5dfda0e1673c3a01ac4b8a
Edward <116730069+Edward-simsim@users.noreply.github.com>
2023-06-26 17:14:19 +0300

    rezolvat erori dupa merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { ForumPageComponent } from './pages/forum-page/forum-page.component';
-import { CreatePageComponent } from './pages/create-page/create-page.component';
-import { QuestionComponent } from './components/question/question.component';
-import { QuestionService } from './service/question/question.service';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-import { ViewQuestionPageComponent } from './pages/view-question-page/view-question-page.component';
-import { CompleteQuestionComponent } from './components/complete-question/complete-question.component';
-import { CategoryComponent } from './components/category/category.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { MembersPageComponent } from './pages/members-page/members-page.component';
-import { CommentComponent } from './components/comment/comment.component';
-import { GoogleLoginProvider, GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
-import { MatSelectModule } from '@angular/material/select';
-import { SocialLoginModule } from '@abacritt/angularx-social-login';
-import { SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SearchPageComponent } from './pages/search-page/search-page.component';
-
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { ForumPageComponent } from "./pages/forum-page/forum-page.component";
+import { CreatePageComponent } from "./pages/create-page/create-page.component";
+import { QuestionComponent } from "./components/question/question.component";
+import { QuestionService } from "./service/question/question.service";
+import { HttpClientModule } from "@angular/common/http";
+import { CommonModule } from "@angular/common";
+import { ViewQuestionPageComponent } from "./pages/view-question-page/view-question-page.component";
+import { CompleteQuestionComponent } from "./components/complete-question/complete-question.component";
+import { CategoryComponent } from "./components/category/category.component";
+import { AboutPageComponent } from "./pages/about-page/about-page.component";
+import { MembersPageComponent } from "./pages/members-page/members-page.component";
+import { CommentComponent } from "./components/comment/comment.component";
+import {
+  GoogleLoginProvider,
+  GoogleSigninButtonModule,
+} from "@abacritt/angularx-social-login";
+import { MatSelectModule } from "@angular/material/select";
+import { SocialLoginModule } from "@abacritt/angularx-social-login";
+import { SocialAuthServiceConfig } from "@abacritt/angularx-social-login";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { SearchPageComponent } from "./pages/search-page/search-page.component";
 
 @NgModule({
   declarations: [
     AppComponent,
 
+    LoginComponent,
     HomePageComponent,
     ForumPageComponent,
     CreatePageComponent,
@@ -53,14 +56,11 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     HttpClientModule,
     CommonModule,
     MatFormFieldModule,
-    GoogleSigninButtonModule
+    GoogleSigninButtonModule,
   ],
-  
-  providers: [
-    QuestionService,
-   
-  ],
+
+  providers: [QuestionService],
   bootstrap: [AppComponent],
-  exports: [QuestionComponent]
+  exports: [QuestionComponent],
 })
-export class AppModule { }
+export class AppModule {}
